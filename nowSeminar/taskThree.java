555b2c46800700930c13c317cb1d1cc839f9fe2e
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-06-01 08:08:57 +0300

    Сделал задачу 3

--- nowSeminar/taskThree.java
+++ nowSeminar/taskThree.java
@@ -1,10 +1,12 @@
+import java.util.function.DoubleToIntFunction;
+
 public class TaskThree {
     public static void main(String[] args) {
-        
+        System.out.println(triangNum(3));
     }
     
-    static public int triangNum()
+    static public int triangNum(int num)
     {
-        return 0;
+        return (int) (num * 0.5 * (num + 1));
     }
 }
