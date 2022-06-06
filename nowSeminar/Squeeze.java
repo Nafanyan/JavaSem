2a3ec6bdebe4fd0add0c32b0acf79ddbadb4ce1b
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-06-06 18:24:34 +0300

    create file Squeeze.java

---
+++ nowSeminar/Squeeze.java
@@ -0,0 +1,10 @@
+public class Squeeze {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void squeString(String source, int index, String result)
+    {
+        
+    }
+}
