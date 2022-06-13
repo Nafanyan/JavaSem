d152bed81dbd17ea54f556f5f24c4f121c231f3b
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-06-13 14:08:54 +0300

    Сделал задачу с Ханойской бащней

--- Sem3/Tower.java
+++ Sem3/Tower.java
@@ -1,6 +1,18 @@
-public class Tower
-{
+import java.util.Arrays;
+
+public class Tower {
     public static void main(String[] args) {
-        
+        int numRings = 2;
+        transferRings(numRings, "Source", "Result", "Buffer");
     }
+
+
+    public static void transferRings( int numRings, String one, String two, String three){
+        if (numRings == 0) return;
+        transferRings(numRings - 1, one, three, two);
+        System.out.println("ring " + numRings + " from " + one + " to " + two);
+        transferRings(numRings - 1, three, two, one);
+
+    } 
+
 }
