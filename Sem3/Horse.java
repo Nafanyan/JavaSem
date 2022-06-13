32b1084c140e68c75e237dc5dbb37aa079a74492
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-06-13 19:08:37 +0300

    add leetcode

--- Sem3/Horse.java
+++ Sem3/Horse.java
@@ -10,7 +10,7 @@ public class Horse {
                 { 2, 1 }, { 2, -1 },
                 { -1, -2 }, { -1, -2 } };
         int[][] board = new int[n][n];
-        doMoveHorse(board, horseDo, {0,0}, 0);
+        
         for (int i = 0; i < n; i++)
         {
             for(int j = 0; j < n; j++)
