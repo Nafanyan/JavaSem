e8f089152b4e8bb256ffd941eb372d849d4471a8
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-06-15 12:14:28 +0300

    Создал класс для парковочной системы

---
+++ Leetcode/parksystem/ParkingSystem.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParkingSystem {
+    private int smallPark;
+    private int mediumPark;
+    private int bigPark;
+
+    public ParkingSystem(int big, int medium, int small) {
+        smallPark = small;
+        mediumPark = medium;
+        bigPark = big;
+    }
+
+    public boolean addCar(int carType) {
+        if (carType == 1) return placeFree (bigPark--);
+        if (carType == 2) return placeFree (mediumPark--);
+        if (carType == 3) return placeFree (smallPark--);
+        return false;
+    }
+
+    private boolean placeFree (int placeNum){
+        return placeNum > 0;
+    }
+    public void getInfo(){
+        System.out.println(String.format("Big %d, Med %d, Small %d", bigPark, mediumPark, smallPark));
+    }
+}
